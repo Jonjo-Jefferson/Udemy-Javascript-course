46bd7cf90eae7e57093ebedda1e7200521247fa0
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2023-05-28 19:05:59 +0100

    init

---
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,17 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks =
+  "That massive" +
+  myNoun +
+  "wqdfq" +
+  myAdjective +
+  "asdad" +
+  myVerb +
+  "dasdad" +
+  myAdverb +
+  "asdadsa"; // Change this line
+// Only change code above this line
