c9dcb95c9b574f2a286e5daed5c380598d60963f
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2023-05-28 19:32:47 +0100

    hi

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,16 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+currentResult = add(1, 2);
+add(5, 5);
+
+currentResult;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
